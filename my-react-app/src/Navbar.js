91649067dcc6aa95022c65d3d5c9372609e87b6f
rithick <rithick502@gmail.com>
2024-01-18 17:15:48 +0530

    dockerized both front-end and back-end

--- my-react-app/src/Navbar.js
+++ my-react-app/src/Navbar.js
@@ -7,9 +7,6 @@ export default function Navbar() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     <div className="container-fluid">
     <a className="navbar-brand" href="/">CRUD APP</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
 
     <Link
       className="btn btn-outline-light" to ="/adduser">Add User
